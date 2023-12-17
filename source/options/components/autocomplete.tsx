26534c48978a6442a6c7f2752661eb7b8ed0eb26
Bauke <me@bauke.xyz>
2023-12-17 13:28:21 +0100

    Add instructions for how to autocomplete.

--- source/options/components/autocomplete.tsx
+++ source/options/components/autocomplete.tsx
@@ -7,6 +7,10 @@ export function AutocompleteSetting(props: SettingProps): JSX.Element {
       <p class="info">
         Adds autocompletion in textareas for user mentions (starting with{" "}
         <code>@</code>) and groups (starting with <code>~</code>).
+        <br />
+        When an autocompletion list is shown you can press <code>Tab</code> to
+        move down the list and <code>Shift+Tab</code> to move up the list. To
+        fill in your highlighted match press <code>Enter</code>.
       </p>
     </Setting>
   );
